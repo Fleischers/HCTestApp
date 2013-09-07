41e5c579876d07b196711420331c2d9ebd1741d8
Fleischers <Fleischer458@gmail.com>
2013-09-08 01:57:37 +0300

    Lessons from Netty.io

---
+++ src/hc/netty/server/lessons/DiscardServerHandler.java
@@ -0,0 +1,27 @@
+package hc.netty.server.lessons;
+
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.util.ReferenceCountUtil;
+
+/**
+ * Handles a server-side channel.
+ */
+public class DiscardServerHandler extends ChannelInboundHandlerAdapter { // (1)
+
+	@Override
+	public void channelRead(ChannelHandlerContext ctx, Object msg) {
+	    try {
+	        // Do something with msg
+	    } finally {
+	        ReferenceCountUtil.release(msg);
+	    }
+	}
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) { // (4)
+        // Close the connection when an exception is raised.
+        cause.printStackTrace();
+        ctx.close();
+    }
+}
