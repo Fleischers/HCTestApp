f42736dfc49ec2a6a70b18405d42f682322824fc
Fleischers <Fleischer458@gmail.com>
2013-09-15 01:36:35 +0300

    Added "/hello" function
    Some practise also

--- src/hc/netty/server/lessons/DiscardServerHandler.java
+++ src/hc/netty/server/lessons/DiscardServerHandler.java
@@ -1,8 +1,9 @@
 package hc.netty.server.lessons;
 
+import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
-import io.netty.util.ReferenceCountUtil;
+
 
 /**
  * Handles a server-side channel.
@@ -11,11 +12,21 @@ public class DiscardServerHandler extends ChannelInboundHandlerAdapter { // (1)
 
 	@Override
 	public void channelRead(ChannelHandlerContext ctx, Object msg) {
+		ByteBuf in = (ByteBuf) msg;
+		
+	    //*
 	    try {
-	        // Do something with msg
+	    	System.out.println(in.toString(io.netty.util.CharsetUtil.US_ASCII));
+	    	
 	    } finally {
-	        ReferenceCountUtil.release(msg);
+	    	/*
+	    	ctx.write(msg); // (1)
+		    ctx.flush(); // (2)
+	    	*/
+	    	in.release(); // (2)
+	    	
 	    }
+	    //*/
 	}
 
     @Override
---
+++ src/hc/netty/server/lessons/TimeServerHandler.java
@@ -0,0 +1,35 @@
+package hc.netty.server.lessons;
+
+
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelFutureListener;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+
+public class TimeServerHandler extends ChannelInboundHandlerAdapter {
+
+    @Override
+    public void channelActive(final ChannelHandlerContext ctx) { // (1)
+        final ByteBuf time = ctx.alloc().buffer(4); // (2)
+        time.writeInt((int) (System.currentTimeMillis() / 1000L + 2208988800L));
+        //Date date = new Date();
+        final ChannelFuture f = ctx.write(time); // (3)
+        //ctx.write(date.toString());
+        ctx.flush();
+        f.addListener(new ChannelFutureListener() {
+            @Override
+            public void operationComplete(ChannelFuture future) {
+                assert f == future;
+                ctx.close();
+            }
+        }); // (4)
+    }
+    
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
+        cause.printStackTrace();
+        ctx.writeAndFlush(cause.toString());
+        ctx.close();
+    }
+}
