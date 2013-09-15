bde02bff0b871c1346eb4883d573537da1f5c9e8
Fleischers <Fleischer458@gmail.com>
2013-09-15 04:51:26 +0300

    Basic feature as "/hello"

---
+++ launchHCServer.bat
@@ -0,0 +1,7 @@
+title HCServer test
+echo off
+color 02
+cls
+echo The Matrix is here.. 
+java -jar target\HCServer-1.0-SNAPSHOT-jar-with-dependencies.jar
+pause
---
+++ src/main/java/hc/fcl/server/HCServer.java
@@ -0,0 +1,66 @@
+package hc.fcl.server;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.codec.http.HttpObjectAggregator;
+import io.netty.handler.codec.http.HttpServerCodec;
+
+import java.util.logging.Logger;
+
+
+
+public class HCServer {
+	private int port;
+	private static final Logger logger = Logger.getLogger(HCServer.class.getName());
+	
+	public HCServer (int port) {
+		this.port=port;
+	}
+	
+	public void run () throws Exception {
+		EventLoopGroup BossGroup = new NioEventLoopGroup();
+		EventLoopGroup WorkerGroup = new NioEventLoopGroup();
+		
+		try {
+			ServerBootstrap b = new ServerBootstrap();
+			b.group(BossGroup, WorkerGroup);
+			b.channel(NioServerSocketChannel.class);
+			b.childHandler(new ChannelInitializer<SocketChannel>() {
+				@Override
+				public void initChannel(SocketChannel ch) throws Exception {
+					ChannelPipeline pipeline = ch.pipeline();
+					pipeline.addLast(new HttpServerCodec());
+					pipeline.addLast(new HttpObjectAggregator(65536));
+					pipeline.addLast(new HCServerHandler());
+				}
+			});
+						
+			ChannelFuture f = b.bind(port).sync();
+			logger.info("Server is listening at http://localhost:"+port+"/");
+			f.channel().closeFuture().sync();
+			
+		}
+		finally {
+			WorkerGroup.shutdownGracefully();
+			BossGroup.shutdownGracefully();
+		}
+		
+	}
+	
+	public static void main (String[] args) throws Exception {
+		int port;
+		if (args.length>0)
+		{
+			port = Integer.parseInt(args[0]);
+		}
+		else port = 8080;
+		new HCServer(port).run();
+	}
+
+}
